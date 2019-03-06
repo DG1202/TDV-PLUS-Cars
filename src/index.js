c24b12edafdd48fbb6eb392adca43468b8f023fe
Dmytro Gnatyshyn <di1202@ukr.net>
2019-03-07 00:44:04 +0200

    webpack changes

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import avg from './some.js';
+
+
+console.log(avg(1,3,5,7,9));
+
+//import './css/style.css';s
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+let path = require('path');
+
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+let conf = {
+    entry: './src/index.js',
+    output: {
+    		path: path.resolve(__dirname, './dist/'),
+		    filename: 'main.js',
+		    publicPath: 'dist/'
+		},
+    devServer: {
+    	overlay: true,
+		},
+    module: {
+    	rules: [
+			{
+				test: /\.js$/,
+	      loader: 'babel-loader',
+				exclude: '/node modules/'
+			}
+			]
+		},
+		plugins: [
+			new ExtractTextPlugin("styles.css"),
+    ]
+
+};
+
+module.exports = (env,option) => {
+     let production = option.mode === 'production';
+     conf.devtool = production ? false : 'eval-sourcemap';
+     return conf;
+};
